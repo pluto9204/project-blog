c1371e67e4d97e52a210dcc4751ed84ef70c3098
kim heejoon <kpluto9204@naver.com>
2022-02-07 16:06:32 +0900

    commit

---
+++ styling-react/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+import SassComponent from './SassComponent.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SassComponent />
+      </div>
+    )
+  }
+}
+
+export default App;
